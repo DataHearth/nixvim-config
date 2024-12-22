6725b53e7cf10c8cfcfc8aecb7dd4c66812b09bb
DataHearth <dev@antoine-langlois.net>
2024-12-22 13:37:40 +0100

    feat: add github action yaml schema

--- plugins/lsp.nix
+++ plugins/lsp.nix
@@ -31,6 +31,7 @@
         settings = {
           schemas = {
             "https://json.schemastore.org/kestra-0.19.0.json" = "kestra/**/*.{yaml,yml}";
+            "https://json.schemastore.org/github-action.json" = ".github/workflows/*.{yaml,yml}";
           };
           schemaStore.enable = true;
         };
